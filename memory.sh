b6f0f3563ba754d7a7ad4ca37a70a71cbd417f6e
Jacob Steward <JacobTheEldest@gmail.com>
2015-07-22 14:43:08 +0000

    Outlined goals for feature

--- memory.sh
+++ memory.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+#goal is to display each module's values in this order:
+#\n
+#Bank Locator:
+#Locator:
+#Size:
+#Form Factor:
+#Type:
+#Speed:
+
+#Then have the script convert:
+#Size from MB to GB where appropriate
+#Type from DDRx to PCx
+#Speed from 'Data transfers per second in millions' to 'Peak transfer rate in MB/s' (*8)
+
 dmidecode -t memory | grep -E "Locator|Size|Form\ Factor|Type:|Speed" | grep -v "Correction"
