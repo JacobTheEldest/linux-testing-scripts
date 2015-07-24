1019fe0d6dac25e3344c6a9aac8fa0a3920ec4f3
Jacob Steward <JacobTheEldest@gmail.com>
2015-07-24 19:59:42 +0000

    Made cd.sh universal

--- cd.sh
+++ cd.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-while [ 1 ]
-do
-	ls /media/ubuntu/
-done
+if [ -d "/media/ubuntu/" ]; then
+	while [ 1 ]; do
+		ls /media/ubuntu/
+	done
+else
+	while [ 1 ]; do
+		ls /media/
+	done
+fi
