50f40a5f4c52fcbcc6a1123f800d85053c896e20
Jacob Steward <JacobTheEldest@gmail.com>
2015-07-29 15:13:04 +0000

    cd.sh now ejects automatically and displays the name of the CD.

--- cd.sh
+++ cd.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+./eject.sh
+echo "Insert CD and close tray."
+
 cddir=""
 
 while [ "$cddir" = "" ]; do
-	cddir=$(lsblk | grep sr0 | awk {'print $7'})
+	cddir=$(lsblk | grep sr0 | awk {'print $7'} | awk -F '[/]' {'print $NF'})
 done
 
-ls $cddir
+echo "Name of CD: $cddir"
+echo "Pass"
+sleep 1
+./eject.sh
