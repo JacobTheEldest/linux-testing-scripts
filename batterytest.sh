3137544a10bd5a1e4e4e00182999d1f7bd29b179
Jacob Steward <JacobTheEldest@gmail.com>
2015-07-30 15:54:26 +0000

    Made batterytest.sh stress the system and show battery info automatically

--- batterytest.sh
+++ batterytest.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-./batteryinfo.sh; sleep 300; killall yes; ./batteryinfo.sh
+cpus=$(lscpu | grep -v "NUMA" | grep "CPU(s)": | awk {'print $2'})
+i="0"
+
+while [ $i -lt $cpus ]; do
+	./stress.sh
+	i=$((i + 1))
+done
+
+./batteryinfo.sh
+sleep 300
+killall yes
+./batteryinfo.sh
