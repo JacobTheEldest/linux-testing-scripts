c8c7a7f38a7987d96d4bd9b0a1e275f915086e8a
Jacob Steward <JacobTheEldest@gmail.com>
2015-07-31 15:27:16 +0000

    Added system serial number QR code generator.

---
+++ qrsysteminfo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Write to variables
+serial=$(dmidecode -t 1 | grep "Serial\ Number" | awk '{print $NF}')
+
+# Encodes the variables field into appropriate files
+if [ ! -d /tmp/systeminfo/ ]; then
+	mkdir /tmp/systeminfo
+fi
+rm /tmp/systeminfo/* #Clear the directory before creating new files
+qrencode -l H -o /tmp/systeminfo/serial.png $serial
+
+# Display QR Codes
+echo "You can cycle through the QR Codes with the arrow keys."
+eog /tmp/systeminfo/serial.png
