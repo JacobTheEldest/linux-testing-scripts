a9aca33db4cab061cf38870b031349293bcfbaeb
Jacob Steward <JacobTheEldest@gmail.com>
2015-08-03 17:16:48 +0000

    Cleaning writelog.sh logic

--- writelog.sh
+++ writelog.sh
@@ -2,6 +2,7 @@
 
 sysserial=$(dmidecode -t 1 | grep Serial)
 git pull
+changed="no"
 
 if grep -Fxq "$sysserial" ./logcpu.txt > /dev/null; then
 	echo "Serial already exists in logcpu.txt"
@@ -10,6 +11,7 @@ else
 	./cpu.sh >> logcpu.txt
 	echo >> logcpu.txt
 	echo "Entry add to logcpu.txt"
+	changed="yes"
 fi
 
 if grep -Fxq "$sysserial" ./loggraphics.txt > /dev/null; then
@@ -19,8 +21,12 @@ else
 	./graphics.sh >> loggraphics.txt
 	echo >> loggraphics.txt
 	echo "Entry added to loggraphics.txt"
+	changed="yes"
 fi
 
-git add logcpu.txt loggraphics.txt
-git commit -m "Added $sysserial to log files"
-git push
+if [ $changed = "yes" ]; then
+	echo "Preparing to push log updates."
+	git add logcpu.txt loggraphics.txt
+	git commit -m "Added $sysserial to log files"
+	git push
+fi
