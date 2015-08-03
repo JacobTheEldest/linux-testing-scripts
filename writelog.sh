5815c65566b43a1e2916d13d049397150b2a0bc8
Jacob Steward <JacobTheEldest@gmail.com>
2015-08-03 17:10:23 +0000

    Prevent duplicate serials in logs

--- writelog.sh
+++ writelog.sh
@@ -3,13 +3,23 @@
 sysserial=$(dmidecode -t 1 | grep Serial)
 git pull
 
-$sysserial >> logcpu.txt
-./cpu.sh >> logcpu.txt
-echo >> logcpu.txt
+if grep -Fxq "$sysserial" ./logcpu.txt > /dev/null; then
+	echo "Serial already exists in logcpu.txt"
+else
+	echo "$sysserial" >> logcpu.txt
+	./cpu.sh >> logcpu.txt
+	echo >> logcpu.txt
+	echo "Entry add to logcpu.txt"
+fi
 
-$sysserial >> loggraphics.txt
-./graphics.sh >> loggraphics.txt
-echo >> loggraphics.txt
+if grep -Fxq "$sysserial" ./loggraphics.txt > /dev/null; then
+	echo "Serial already exists in loggraphics.txt"
+else
+	echo "$sysserial" >> loggraphics.txt
+	./graphics.sh >> loggraphics.txt
+	echo >> loggraphics.txt
+	echo "Entry added to loggraphics.txt"
+fi
 
 git add logcpu.txt loggraphics.txt
 git commit -m "Added $sysserial to log files"
