1f80523c6a926d55291316dbed0a72f93bcfdb28
Jacob Steward <JacobTheEldest@gmail.com>
2015-08-03 15:45:35 +0000

    Neatened log outputs. Automated git syncing of log files.

--- writelog.sh
+++ writelog.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-dmidecode -t 1 | grep Serial >> logcpu.txt
-./cpu.sh >> logcpu.txt
+sysserial=$(dmidecode -t 1 | grep Serial)
+git pull
 
-dmidecode -t 1 | grep Serial >> loggraphics.txt
+$sysserial >> logcpu.txt
+./cpu.sh >> logcpu.txt
+echo >> logcpu.txt
+
+$sysserial >> loggraphics.txt
 ./graphics.sh >> loggraphics.txt
+echo >> loggraphics.txt
+
+git add logcpu.txt loggraphics.txt
+git commit -m "Added $sysserial to log files"
+git push
