44a1aa3accfe5fc43859bb5b71e7ad26657e0dba
root <root@localhost.localdomain>
2015-08-06 05:39:28 -0500

    Added cdrkit to installprogs.sh

--- installprogs.sh
+++ installprogs.sh
@@ -5,9 +5,9 @@ program_exists () {
 }
 
 if program_exists pacman; then
-    pacman -S --noconfirm git mplayer qrencode bc fswebcam eog hdparm lshw alsa-utils
+    pacman -S --noconfirm git mplayer qrencode bc fswebcam eog hdparm lshw alsa-utils cdrkit
 elif program_exists apt-get; then
-    apt-get install -y git mplayer qrencode bc fswebcam eog hdparm lshw alsa-utils
+    apt-get install -y git mplayer qrencode bc fswebcam eog hdparm lshw alsa-utils cdrkit
 else
-    echo "Cannot detect Package Manager. Install git, alsa-utils, lshw, mplayer, bc, eog, hdparm, fswebcam, and qrencode manually."
+    echo "Cannot detect Package Manager. Install git, cdrkit, alsa-utils, lshw, mplayer, bc, eog, hdparm, fswebcam, and qrencode manually."
 fi
