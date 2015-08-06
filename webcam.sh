b259e95f7a635c847a0be8139eeeb585adf42d5c
root <root@localhost.localdomain>
2015-08-06 03:22:22 -0500

    webcam.sh creates test directory if it does not exist. Opens files after captured.

--- webcam.sh
+++ webcam.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-fswebcam /tmp/webcam/test.png
+if [ ! -d /tmp/webcam/ ]; then
+	mkdir /tmp/webcam
+fi
+rm -f /tmp/webcam/* #Clear the directory before creating new files
+
+fswebcam /tmp/webcam/test.jpeg
+eog /tmp/webcam/test.jpeg
