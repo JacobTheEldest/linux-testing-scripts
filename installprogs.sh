74c2c048a1a9d907376c3200eb0b4e5c3a5965ee
root <root@localhost.localdomain>
2015-08-06 03:30:21 -0500

    Added more necessary programs to installprogs.sh

--- installprogs.sh
+++ installprogs.sh
@@ -5,9 +5,9 @@ program_exists () {
 }
 
 if program_exists pacman; then
-    pacman -S --noconfirm git mplayer qrencode bc fswebcam eog hdparm
+    pacman -S --noconfirm git mplayer qrencode bc fswebcam eog hdparm lshw alsa-utils
 elif program_exists apt-get; then
-    apt-get install -y git mplayer qrencode bc fswebcam eog hdparm
+    apt-get install -y git mplayer qrencode bc fswebcam eog hdparm lshw alsa-utils
 else
-    echo "Cannot detect Package Manager. Install git, mplayer, bc, eog, hdparm, fswebcam, and qrencode manually."
+    echo "Cannot detect Package Manager. Install git, alsa-utils, lshw, mplayer, bc, eog, hdparm, fswebcam, and qrencode manually."
 fi
