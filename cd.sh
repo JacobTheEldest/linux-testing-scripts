0582c3b80a22244a88576401d0712b193a61b5c6
root <root@localhost.localdomain>
2015-08-06 06:07:39 -0500

    made cd.sh not require mounting.

--- cd.sh
+++ cd.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 ./eject.sh
+echo
 echo "Insert CD and close tray."
 
 cddir=""
-
 while [ "$cddir" = "" ]; do
-	cddir=$(lsblk | grep sr0 | awk {'print $7'} | awk -F '[/]' {'print $NF'})
+	cddir=$(dd status=none if=/dev/sr0 bs=1 skip=32808 count=32 2>/dev/null)
 done
 
+echo
 echo "Name of CD: $cddir"
 echo "Pass"
 sleep 1
