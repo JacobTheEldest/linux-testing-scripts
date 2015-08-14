55b61e8f039249d71e6f7d454d6064219f1b62c6
Jacob Steward <JacobTheEldest@gmail.com>
2015-08-14 12:35:54 +0000

    Maxed volume for a greater variety of speakers with audio.sh.

--- audio.sh
+++ audio.sh
@@ -2,6 +2,12 @@
 
 amixer sset 'Master' unmute
 amixer sset 'Master' 100%
+amixer sset 'Front' unmute
 amixer sset 'Front' 100%
+amixer sset 'Headphone' unmute
 amixer sset 'Headphone' 100%
+amixer sset 'Speaker' unmute
+amixer sset 'Speaker' 100%
+amixer sset 'Bass Speaker' unmute
+amixer sset 'Bass Speaker' 100%
 mplayer ./sound.mp3
