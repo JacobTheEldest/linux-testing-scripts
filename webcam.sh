88f6bd27a83b078a8c7741dc92e068f264720469
Jacob <JacobTheEldest@gmail.com>
2015-12-21 07:59:43 -0500

    webcam.sh no longer displays the image captured.

--- webcam.sh
+++ webcam.sh
@@ -5,5 +5,9 @@ if [ ! -d /tmp/webcam/ ]; then
 fi
 rm -f /tmp/webcam/* #Clear the directory before creating new files
 
-fswebcam /tmp/webcam/test.jpeg
-eog /tmp/webcam/test.jpeg
+fswebcam /tmp/webcam/test.jpeg 2> /dev/null
+if [ -a /tmp/webcam/test.jpeg ]; then
+	echo "Pass!"
+else
+	echo "Fail!"
+fi
