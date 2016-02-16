b28791dbfd1e168b75163e684c51365e9f574281
Jacob <JacobTheEldest@gmail.com>
2016-02-16 08:26:37 -0500

    updated to use 'cat /proc/cpuinfo' instead of 'lscpu'

--- cpu.sh
+++ cpu.sh
@@ -1,29 +1,48 @@
 #!/bin/bash
 
+#Trim leading and trailing whitespace
+function trim () {
+    trimmed=$@
+    trimmed="${trimmed#"${trimmed%%[![:space:]]*}"}"
+    trimmed="${trimmed%"${trimmed##*[![:space:]]}"}"
+    echo "$trimmed"
+}
+
 #Initial info from lscpu
-main=$(lscpu | grep "Model\ name:")
-cores=$(lscpu | grep "socket:" | awk {'print $NF'})
+main=$(cat /proc/cpuinfo | grep -i -m 1 "Model\ name")
+cores=$(cat /proc/cpuinfo | grep -i -m 1 "cpu cores" | awk {'print $NF'})
 
 #Strip undesirable characters from cpu string
-main=${main/Model name: /}
+main=${main/model name/}
+main=${main/:/}
 main=${main/CPU/}
 main=${main/(R)/}
 main=${main/(TM)/\ }
 main=${main/-/\ }
 main=${main/@/}
 
-#define variables and strip from main string
-#speed
+# define variables and strip from main string
 speed=$(echo $main | awk {'print $NF'})
 main=${main/$speed/}
+speed=$(echo ${speed:0:-3} ${speed: -3})
+
 #manufacturer
 manufacturer=$(echo $main | awk '{print $1}')
 main=${main/$manufacturer/}
+
 #model number
-model_num=$(echo $main | awk {'print $NF'})
-main=${main/$model_num/}
-#model
-model=$(echo $main | xargs)
+identifier=$(echo $main | awk '{print $(NF-1)}')
+if [ ${#identifier} -eq 1 ]; then
+    model_num="$(echo $main | awk '{print $NF}')$identifier"
+    main=$(echo $main | awk '{$NF=$(NF-1)=""; print $0}')
+else
+    model_num=$(echo $main | awk {'print $NF'})
+    main=${main/$model_num/}
+fi
+
+#model (trimming leading and trailing whitespace)
+model=$(trim $main)
+
 
 #output
 echo
