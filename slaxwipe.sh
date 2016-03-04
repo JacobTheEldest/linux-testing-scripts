cd9327c4d1489b256a15e112f0b1bd1e3ebc9e9a
Jacob <JacobTheEldest@gmail.com>
2016-03-04 02:50:34 -0500

    fixed syntax error cause by typo.

--- slaxwipe.sh
+++ slaxwipe.sh
@@ -9,7 +9,7 @@ if [ -z $1 ]; then
 fi
 
 slaxdev=$(lsblk | grep "/mnt/live/memory/data")
-slaxdev=$(echo "{slaxdev:2: -1}"
+slaxdev=$(echo "{slaxdev:2: -1}")
 
 #if $1 is the slax live device
 if [ $1 = $slaxdev ]; then
@@ -18,4 +18,3 @@ if [ $1 = $slaxdev ]; then
 else
     dd bs=4M if=/dev/zero of=/dev/$1 && sync
 fi
-
