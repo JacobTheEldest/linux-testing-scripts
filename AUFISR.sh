1061110249f0d5c166d8546a6a76398b9c80bd32
Jacob <JacobTheEldest@gmail.com>
2016-03-10 10:03:18 -0500

    Now clears pacman cache before replicating.

--- AUFISR.sh
+++ AUFISR.sh
@@ -6,6 +6,11 @@ if [ -z $1 ]; then
     exit 1
 fi
 
+# Clear source media pacman cache to reduce size
+echo "Clearing pacman cache to reduce size"
+pacman -Scc --noconfirm
+echo
+
 # Partition Target Device
 echo "Partitioning target device"
 parted /dev/${1} mklabel msdos
@@ -14,21 +19,24 @@ parted /dev/${1} set 1 boot on
 mkfs.ext4 -O "^has_journal" /dev/${1}1
 
 # Copy entire filesystem
+echo
 echo "Copying filesystem"
+echo
 mount /dev/${1}1 /mnt/
-echo "Copying (1/14)"; cp -rp /bin /mnt/
-echo "Copying (2/14)"; cp -rp /boot /mnt/
-echo "Copying (3/14)"; cp -rp /etc /mnt/
-echo "Copying (4/14)"; cp -rp /home /mnt/
-echo "Copying (5/14)"; cp -rp /lib /mnt/
-echo "Copying (7/14)"; cp -rp /lost+found /mnt/
-echo "Copying (8/14)"; cp -rp /opt /mnt/
-echo "Copying (9/14)"; cp -rp /root /mnt/
-echo "Copying (10/14)"; cp -rp /run /mnt/
-echo "Copying (11/14)"; cp -rp /sbin /mnt/
-echo "Copying (12/14)"; cp -rp /srv /mnt/
-echo "Copying (13/14)"; cp -rp /usr /mnt/
-echo "Copying (14/14)"; cp -rp /var /mnt/
+echo "Copying (1/13)"; cp -rp /bin /mnt/
+echo "Copying (2/13)"; cp -rp /boot /mnt/
+echo "Copying (3/13)"; cp -rp /etc /mnt/
+echo "Copying (4/13)"; cp -rp /home /mnt/
+echo "Copying (5/13)"; cp -rp /lib /mnt/
+echo "Copying (6/13)"; cp -rp /lost+found /mnt/
+echo "Copying (7/13)"; cp -rp /opt /mnt/
+echo "Copying (8/13)"; cp -rp /root /mnt/
+echo "Copying (9/13)"; cp -rp /run /mnt/
+echo "Copying (10/13)"; cp -rp /sbin /mnt/
+echo "Copying (11/13)"; cp -rp /srv /mnt/
+echo "Copying (12/13)"; cp -rp /usr /mnt/
+echo "Copying (13/13)"; cp -rp /var /mnt/
+echo
 mkdir /mnt/mnt
 mkdir /mnt/dev
 mkdir /mnt/proc
@@ -41,7 +49,8 @@ uuid=$(blkid | grep ${1}1)
 uuid=${uuid/"/dev/${1}1: "/}
 uuid=$(echo $uuid | awk {'print $1'})
 uuid=${uuid//\"/}
-echo "$uuid	/         	ext4      	rw,relatime	0 1" > /mnt/target/etc/fstab
+echo "$uuid	/         	ext4      	rw,relatime	0 1" > /mnt/etc/fstab
+echo
 
 # Install Grub
 echo "Installing GRUB"
@@ -51,6 +60,7 @@ mount --rbind /sys sys/
 mount --rbind /dev dev/
 chroot ./ grub-install --recheck /dev/${1}
 chroot ./ grub-mkconfig -o /boot/grub/grub.cfg
+echo
 
 # Updating target scripts
 echo "Updating target scripts"
