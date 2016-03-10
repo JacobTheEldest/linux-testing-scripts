ab8052d90323ee9fc56787aba9648ac030fe47c6
Jacob <JacobTheEldest@gmail.com>
2016-03-10 07:14:51 -0500

    Pared down to pertinent info. Uses lshw.

--- network.sh
+++ network.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 
-lshw -class network | grep -iE "description|vendor|product"
+#count the number of slots (empty and full)
+#total=$(dmidecode -t memory | grep "Number Of Devices:" | awk {'print $4'})
+total=$(lshw -class network | grep "description" | wc -l)
+
+echo
+echo "Number of interfaces: $total"
+echo
+lshw -class network | grep -iE -m 4 "description|vendor|product|capabilities"
+echo
+lshw -class network | grep -iE "description|vendor|product|capabilities" | tail -4
+echo
