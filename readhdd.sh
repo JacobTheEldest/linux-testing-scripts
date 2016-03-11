28b80ca48b09a747b8652f8f6c751165bb4a119e
Jacob <JacobTheEldest@gmail.com>
2016-03-11 08:28:40 -0500

    Now asks for a device name instead of automatically choosing sda.

--- readhdd.sh
+++ readhdd.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-strings /dev/sda | less
+#Ensure that there is a valid argument
+if [ -z $1 ]; then
+    echo "Enter the target device as an argument"
+    exit 1
+fi
+
+strings /dev/$1| less
